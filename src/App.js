0afa5369e821d805437f8b35bf34c29c4a563751
Nick Boyd <nickjohnboyd@gmail.com>
2023-04-19 16:50:38 -0600

    added state to newhero function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Welcome to Hero Game!</p>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currHeroId: 0
+    };
+    this.newHero = this.newHero.bind(this);
+  }
+  newHero() {
+    this.setState({currHeroId: this.state.currHeroId + 1});
+    console.log("New Hero!: " + this.state.currHeroId);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Welcome to Hero Game!</p>
+        <p>Current Hero: {this.state.currHeroId}</p>
+        <button onClick={this.newHero}>Click Me</button>
+      </div>
+    );
+  }
 }
 
 export default App;
